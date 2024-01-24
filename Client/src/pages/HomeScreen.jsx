e19979de954e329f8a15d9a89abf2cea88f5080c
SHANIB C K <ckshanib8@gmail.com>
2024-01-24 20:19:06 +0530

    Home page just created

--- Client/src/pages/HomeScreen.jsx
+++ Client/src/pages/HomeScreen.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const HomeScreen = () => {
   return (
     <>
-      <div>Home screen</div>
+      <div className="h-screen w-screen flex flex-col items-center justify-center">
+        <h1>Home screen</h1>
+      </div>
     </>
   );
 };
