5de6b0a65eb89cfc114d8c271b4fdb0bd47e8baf
SHANIB C K <ckshanib8@gmail.com>
2024-01-24 20:15:55 +0530

    home screen page just created

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,17 @@
+// importing section
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeScreen from "./pages/HomeScreen";
 
 const App = () => {
   return (
     <>
-      <div>Hello</div>
+      {/* Router creating section */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
