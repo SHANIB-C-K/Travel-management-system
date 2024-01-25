623dd6c2d8a00428b90a3e007ce5c31526ddffdb
SHANIB C K <ckshanib8@gmail.com>
2024-01-25 19:31:45 +0530

    primary and  secondary color created

--- Client/src/components/Navbar/NavBar.jsx
+++ Client/src/components/Navbar/NavBar.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const NavBar = () => {
   return (
     <>
-      <div>Navbar</div>
+      <div className="fixed top-0 w-full right-0 bg-white text-black shadow-sm">
+        <div className="container bg-gradient-to-r from-primary to-secondary">
+          <div className="flex">
+            <p>20% off on next booking</p>
+            <p>Mobile no +91 6282844209</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0287a8",
+        secondary: "#00c3c7",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
   },
   plugins: [],
 };
