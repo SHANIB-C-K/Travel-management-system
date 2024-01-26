98fbed90f39574b401dfabd2179e58e4001220b9
SHANIB C K <ckshanib8@gmail.com>
2024-01-26 19:08:38 +0530

    json file created

--- Client/src/components/Navbar/NavBar.jsx
+++ Client/src/components/Navbar/NavBar.jsx
@@ -1,113 +1,118 @@
 // importing section
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useState } from "react";
 import Logo from "../../assets/logo.png";
+import { NavLink, Link } from "react-router-dom";
 import { FaCaretDown } from "react-icons/fa";
+import ResponsiveMenu from "./ResponsiveMenu";
+import { HiMenuAlt3, HiMenuAlt1 } from "react-icons/hi";
+import DropdownLinks from "../../JSON/DropDownLinks.json";
 
-// dropdown links opject create section
-const DropDownLinks = [
-  { name: "Our Services", link: "/#services" },
-  { name: "Top Brands", link: "/#mobile_brands" },
-  { name: "Location", link: "/#location" },
-];
+const Navbar = ({ handleOrderPopup }) => {
+  const [showMenu, setShowMenu] = useState(false);
 
-const NavBar = () => {
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
   return (
     <>
-      <div className="fixed top-0 w-full right-0 bg-white text-black shadow-sm">
+      <nav className="fixed top-0 right-0 w-full z-50 bg-white backdrop-blur-sm text-black shadow-md">
         <div className="bg-gradient-to-r from-primary to-secondary text-white ">
           <div className="container py-[2px] sm:block hidden">
             <div className="flex items-center justify-between">
               <p className="text-sm">20% off on next booking</p>
-              <p>mobile no. +91 123456789</p>
+              <p>mobile no. +91 6282844209</p>
             </div>
           </div>
         </div>
         <div className="container py-3 sm:py-0">
           <div className="flex justify-between items-center">
-            {/* logo section */}
-            <div>
-              <Link to="/" onClick={() => window.scrollTo(0, 0)}>
-                <img src={Logo} alt="Logo section" className="h-16" />
+            <div className="flex items-center gap-4  font-bold text-2xl">
+              <Link to={"/"} onClick={() => window.scrollTo(0, 0)}>
+                <img src={Logo} alt="" className="h-16" />
               </Link>
+              {/* <span>TCJ Tourism</span> */}
             </div>
-            {/* Desktop size Navbar Link section */}
             <div className="hidden md:block">
-              <ul className="flex items-center gap-6">
+              <ul className="flex items-center gap-6 ">
                 <li className="py-4">
-                  <NavLink
-                    to="/"
-                    activeClassName="active"
-                    onClick={() => window.scrollTo(0, 0)}
-                  >
+                  <NavLink to="/" activeclassname="active">
                     Home
                   </NavLink>
                 </li>
                 <li className="py-4">
-                  <NavLink
-                    to="/blogs"
-                    activeClassName="active"
-                    onClick={() => window.scrollTo(0, 0)}
-                  >
+                  <NavLink to="/blogs" activeclassname="active">
                     Blogs
                   </NavLink>
                 </li>
                 <li className="py-4">
-                  <NavLink
-                    to="/places"
-                    activeClassName="active"
-                    onClick={() => window.scrollTo(0, 0)}
-                  >
+                  <NavLink to="/best-places" activeclassname="active">
                     Best Places
                   </NavLink>
                 </li>
                 <li className="py-4">
-                  <NavLink
-                    to="/about"
-                    activeClassName="active"
-                    onClick={() => window.scrollTo(0, 0)}
-                  >
+                  <NavLink to="/about" activeclassname="active">
                     About
                   </NavLink>
                 </li>
-                {/* Dropdown creating section */}
-                <li className="py-4 relative group cursor-pointer">
-                  <div className="dropdown flex items-center">
-                    <span>Quick Links</span>
+                <li className="group relative cursor-pointer">
+                  <a
+                    href="/#home"
+                    className="flex h-[72px] items-center gap-[2px]"
+                  >
+                    Quick Links{" "}
                     <span>
                       <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
                     </span>
-                  </div>
-                  <div className="absolute -left-9 top-[57px] z-[9999] hidden group-hover:block shadow-md text-black w-[150px] bg-white p-2">
-                    <ul>
-                      {DropDownLinks.map((data) => {
-                        return (
-                          <li key={data.name}>
-                            <a
-                              href={data.link}
-                              className="inline-block w-full rounded-md p-2 hover:bg-primary/20"
-                            >
-                              {data.name}
-                            </a>
-                          </li>
-                        );
-                      })}
+                  </a>
+                  <div className="absolute -left-9 z-[9999] hidden w-[150px] rounded-md bg-white p-2 text-black group-hover:block shadow-md ">
+                    <ul className="space-y-3">
+                      {DropdownLinks.map((data) => (
+                        <li key={data.name}>
+                          <a
+                            className="inline-block w-full rounded-md p-2 hover:bg-primary/20"
+                            href={data.link}
+                          >
+                            {data.name}
+                          </a>
+                        </li>
+                      ))}
                     </ul>
                   </div>
                 </li>
               </ul>
             </div>
-            {/* Book now button section */}
-            <div>
-              <button className="bg-gradient-to-r from-primary to-secondary hover:bg-bg-gradient-to-r hover:from-secondary hover:bg-primary transition-all duration-600 text-white px-3 py-1 rounded-full">
+            <div className="flex items-center gap-4">
+              <button
+                className="bg-gradient-to-r from-primary to-secondary hover:bg-bg-gradient-to-r hover:from-secondary hover:bg-primary transition-all duration-600 text-white px-3 py-1 rounded-full"
+                onClick={() => {
+                  handleOrderPopup();
+                }}
+              >
                 Book Now
               </button>
+              {/* Mobile Hamburger icon */}
+              <div className="md:hidden block">
+                {showMenu ? (
+                  <HiMenuAlt1
+                    onClick={toggleMenu}
+                    className=" cursor-pointer transition-all"
+                    size={30}
+                  />
+                ) : (
+                  <HiMenuAlt3
+                    onClick={toggleMenu}
+                    className="cursor-pointer transition-all"
+                    size={30}
+                  />
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+        <ResponsiveMenu setShowMenu={setShowMenu} showMenu={showMenu} />
+      </nav>
     </>
   );
 };
 
-export default NavBar;
+export default Navbar;
---
+++ Client/src/components/Navbar/ResponsiveMenu.jsx
@@ -0,0 +1,48 @@
+// importing section
+import React from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import NavbarLinks from "../../JSON/NavBarLinks.json";
+
+const ResponsiveMenu = ({ showMenu, setShowMenu }) => {
+  return (
+    <>
+      <div
+        className={`${
+          showMenu ? "left-0" : "-left-[100%]"
+        } fixed bottom-0 top-0 z-20 flex h-screen w-[75%] flex-col justify-between bg-white dark:bg-gray-900 dark:text-white px-8 pb-6 pt-16 text-black transition-all duration-200 md:hidden rounded-r-xl shadow-md`}
+      >
+        <div className="Navbar__card">
+          {/* top section */}
+          <div>
+            <div className="flex items-center justify-start gap-4">
+              <FaUserCircle size={50} className="text-primary" />
+              <div className="text-gray-500">
+                <h1>Hello user</h1>
+                <h1 className="text-sm text-gray-500">Premium user</h1>
+              </div>
+            </div>
+          </div>
+          {/* Navlinks section */}
+          <div className="text-black mt-12">
+            <ul className="space-y-4">
+              {NavbarLinks.map(({ name, link }) => (
+                <li>
+                  <Link
+                    to={link}
+                    onClick={() => setShowMenu(false)}
+                    className="mb-5 inline-block"
+                  >
+                    {name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ResponsiveMenu;
