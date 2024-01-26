2afd1ad952aaafe59c1569832a21a9d8a31a5b11
SHANIB C K <ckshanib8@gmail.com>
2024-01-26 11:16:17 +0530

    Navbar logo added

--- Client/src/components/Navbar/NavBar.jsx
+++ Client/src/components/Navbar/NavBar.jsx
@@ -1,13 +1,29 @@
+// importing section
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.png";
 
 const NavBar = () => {
   return (
     <>
       <div className="fixed top-0 w-full right-0 bg-white text-black shadow-sm">
-        <div className="container bg-gradient-to-r from-primary to-secondary">
-          <div className="flex">
-            <p>20% off on next booking</p>
-            <p>Mobile no +91 6282844209</p>
+        <div className="bg-gradient-to-r from-primary to-secondary text-white ">
+          <div className="container py-[2px] sm:block hidden">
+            <div className="flex items-center justify-between">
+              <p className="text-sm">20% off on next booking</p>
+              <p>mobile no. +91 123456789</p>
+            </div>
+          </div>
+        </div>
+        <div className="container py-3 sm:py-0">
+          <div className="flex justify-between items-center">
+            {/* logo section */}
+            <div>
+              <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+                <img src={Logo} alt="Logo section" className="h-16" />
+              </Link>
+            </div>
+            {/* Navbar Link section */}
           </div>
         </div>
       </div>
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
+        // primary: "#00c3c7",
         primary: "#0287a8",
         secondary: "#00c3c7",
+        dark: "#ffcf22",
       },
       container: {
         center: true,
