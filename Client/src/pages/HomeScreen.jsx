72d4749c77a47a6fd700f22a42326c4282711d5b
SHANIB C K <ckshanib8@gmail.com>
2024-01-26 19:36:36 +0530

    homescreen just added😉

--- Client/src/pages/HomeScreen.jsx
+++ Client/src/pages/HomeScreen.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import NatureVid from "../assets/video/main.mp4";
 
 const HomeScreen = () => {
   return (
     <>
-      <h1>Home screen</h1>
+      <div>
+        <div className="h-[700px] relative">
+          <video
+            autoPlay
+            loop
+            muted
+            className="absolute right-0 top-0 h-[700px] w-full object-cover z-[-1]"
+          >
+            <source src={NatureVid} type="video/mp4" />
+          </video>
+        </div>
+      </div>
     </>
   );
 };
