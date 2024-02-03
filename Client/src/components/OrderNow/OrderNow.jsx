2b73aaa8f374dfd6a27662145e8294b8a327be6b
SHANIB C K <ckshanib8@gmail.com>
2024-02-03 17:23:00 +0530

    data add function create successfully

--- Client/src/components/OrderNow/OrderNow.jsx
+++ Client/src/components/OrderNow/OrderNow.jsx
@@ -1,7 +1,24 @@
+// importing section
 import React from "react";
 import { IoCloseOutline } from "react-icons/io5";
+import { database } from "./../../config/firebase";
+import { collection } from "firebase/firestore";
+import { addDoc } from "firebase/firestore";
 
 const OrderNow = ({ orderPopup, setOrderPopup }) => {
+  // usestate section
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [address, setAddress] = React.useState("");
+
+  // collection create section
+  const value = collection(database, "Booking");
+
+  // HandleBooking function create section
+  const HandleBooking = async () => {
+    await addDoc(value, { name: name, email: email, address: address });
+  };
+
   return (
     <>
       {orderPopup && (
@@ -28,19 +45,28 @@ const OrderNow = ({ orderPopup, setOrderPopup }) => {
                 type="text"
                 placeholder="Name"
                 className="w-full rounded-full border border-gray-300 dark:border-gray-500 dark:bg-gray-800 px-2 py-1 mb-4"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
               <input
                 type="email"
                 placeholder="email"
                 className="w-full rounded-full border border-gray-300 dark:border-gray-500 dark:bg-gray-800 px-2 py-1 mb-4"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
               <input
                 type="text"
                 placeholder="Address"
                 className="w-full rounded-full border border-gray-300 dark:border-gray-500 dark:bg-gray-800 px-2 py-1 mb-4"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
               />
               <div className="flex justify-center">
-                <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-1 px-4 rounded-full ">
+                <button
+                  className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-1 px-4 rounded-full "
+                  onClick={HandleBooking}
+                >
                   Book Now
                 </button>
               </div>
