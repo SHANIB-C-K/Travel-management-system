8c91990cfcb8245de9ccef20b7b4c129e06effea
SHANIB C K <ckshanib8@gmail.com>
2024-02-03 19:19:37 +0530

    order now all page created

--- Client/src/components/Places/PlaceDetails.jsx
+++ Client/src/components/Places/PlaceDetails.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import Places from "./Place";
+import OrderNow from "../OrderNow/OrderNow";
 
 const PlaceDetails = () => {
   const locations = useLocation();
   // console.log(props, " props");
   // console.log(location, " useLocation Hook");
   const { image, title, description } = locations.state;
+  const [orderPopup, setOrderPopup] = React.useState(false);
+
+  // order popup open function created section
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
 
   return (
     <div className="pt-20">
@@ -21,13 +28,17 @@ const PlaceDetails = () => {
         <h1 className="text-2xl font-semibold">{title}</h1>
         <p>{description}</p>
         <div className="items-center justify-center flex sm:mt-16 mt-14 ml-24 ">
-          <button className="bg-gradient-to-r from-primary to-secondary text-white hover:scale-105 px-4 py-2 rounded-full duration-200 absolute -translate-x-1/2">
+          <button
+            className="bg-gradient-to-r from-primary to-secondary text-white hover:scale-105 px-4 py-2 rounded-full duration-200 absolute -translate-x-1/2"
+            onClick={handleOrderPopup}
+          >
             Book Now
           </button>
         </div>
       </div>
       {/* All Blogs Section */}
       <Places />
+      <OrderNow orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
     </div>
   );
 };
--- Client/src/pages/Layout.jsx
+++ Client/src/pages/Layout.jsx
@@ -1,3 +1,4 @@
+// importing section
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
@@ -5,8 +6,10 @@ import OrderNow from "./../components/OrderNow/OrderNow";
 import Navbar from "./../components/Navbar/NavBar";
 
 const Layout = () => {
+  // usestate section
   const [orderPopup, setOrderPopup] = React.useState(false);
 
+  // order popup open function create section
   const handleOrderPopup = () => {
     setOrderPopup(!orderPopup);
   };
