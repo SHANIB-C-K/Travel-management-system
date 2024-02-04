d1954175581ee38c8c2e3a092849ce674dd0dd7d
SHANIB C K <ckshanib8@gmail.com>
2024-02-04 15:51:10 +0530

    order popup added all pages

--- Client/src/pages/BlogDetails.jsx
+++ Client/src/pages/BlogDetails.jsx
@@ -2,12 +2,19 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import BlogsComp from "../components/BlogsComponent/BlogComponent";
+import OrderNow from "../components/OrderNow/OrderNow";
 
 const BlogsDetails = () => {
   const location = useLocation();
   // console.log(props, " props");
   // console.log(location, " useLocation Hook");
   const { image, date, title, description, author } = location.state;
+  const [orderPopup, setOrderPopup] = React.useState(false);
+
+  // order popup open function created section
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
 
   return (
     // html section
@@ -27,13 +34,17 @@ const BlogsDetails = () => {
         <h1 className="text-2xl font-semibold">{title}</h1>
         <p>{description}</p>
         <div className="items-center justify-center flex sm:mt-16 mt-14 ml-24 ">
-          <button className="bg-gradient-to-r from-primary to-secondary text-white hover:scale-105 px-4 py-2 rounded-full duration-200 absolute -translate-x-1/2">
+          <button
+            className="bg-gradient-to-r from-primary to-secondary text-white hover:scale-105 px-4 py-2 rounded-full duration-200 absolute -translate-x-1/2"
+            onClick={handleOrderPopup}
+          >
             Book Now
           </button>
         </div>
       </div>
       {/* All Blogs Section */}
       <BlogsComp />
+      <OrderNow orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
     </div>
   );
 };
