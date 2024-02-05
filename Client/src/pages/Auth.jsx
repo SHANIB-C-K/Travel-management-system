4d4a9e1921de9c648757f649c4d88b22f8e47896
SHANIB C K <ckshanib8@gmail.com>
2024-02-05 19:30:12 +0530

    loading page just added

--- Client/src/pages/Auth.jsx
+++ Client/src/pages/Auth.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { database } from "../config/firebase";
 import {
   createUserWithEmailAndPassword,
@@ -13,6 +13,7 @@ import HomeScreen from "./HomeScreen";
 const Auth = () => {
   // usestate section
   const [login, setLogin] = React.useState(false);
+  const [time, setTime] = React.useState(false);
 
   const databases = getAuth();
 
@@ -98,85 +99,102 @@ const Auth = () => {
     theme: "dark",
   };
 
+  // setTimeout create section
+  setTimeout(() => {
+    setTime(true);
+  }, 1000);
+
   return (
     <>
       {/* html section */}
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            {login ? "Login in to your account" : "Sign up to your account"}
-          </h2>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form
-            className="space-y-6"
-            onSubmit={(e) => HandleSubmit(e, login ? "signin" : "signup")}
-          >
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+      {!time ? (
+        <>
+          <div className="h-screen w-screen flex flex-col items-center justify-center">
+            <h1 class="flex font-semibold text-purple-600">
+              <span class="animate-waving-hand text-9xl">👋🏻</span>
+            </h1>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <img
+                className="mx-auto h-10 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt="Your Company"
+              />
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                {login ? "Login in to your account" : "Sign up to your account"}
+              </h2>
             </div>
 
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form
+                className="space-y-6"
+                onSubmit={(e) => HandleSubmit(e, login ? "signin" : "signup")}
+              >
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Email address
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Password
+                    </label>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    {!login ? "Sign up" : "Login"}
+                  </button>
+                </div>
+              </form>
+
+              <p className="mt-10 text-center text-sm text-gray-500">
+                {login ? "Not a member?  " : "You already a member?  "}
+                <a
+                  onClick={LoginRegister}
+                  className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 cursor-pointer"
                 >
-                  Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+                  {login ? "SignUp" : "Login"}
+                </a>
+              </p>
+              <ToastContainer />
             </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                {!login ? "Sign up" : "Login"}
-              </button>
-            </div>
-          </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            {login ? "Not a member?  " : "You already a member?  "}
-            <a
-              onClick={LoginRegister}
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 cursor-pointer"
-            >
-              {login ? "SignUp" : "Login"}
-            </a>
-          </p>
-          <ToastContainer />
-        </div>
-      </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
--- Client/src/pages/Layout.jsx
+++ Client/src/pages/Layout.jsx
@@ -8,17 +8,36 @@ import Navbar from "./../components/Navbar/NavBar";
 const Layout = () => {
   // usestate section
   const [orderPopup, setOrderPopup] = React.useState(false);
+  const [time, setTime] = React.useState(false);
 
   // order popup open function create section
   const handleOrderPopup = () => {
     setOrderPopup(!orderPopup);
   };
+
+  setTimeout(() => {
+    setTime(true);
+  }, 2000);
   return (
     <>
-      <Navbar handleOrderPopup={handleOrderPopup} />
-      <Outlet />
-      <Footer />
-      <OrderNow orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
+      {!time ? (
+        <>
+          {/* loading section */}
+          <div className="flex space-x-2 justify-center items-center bg-white h-screen dark:invert">
+            <span className="sr-only">Loading...</span>
+            <div className="h-6 w-6 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+            <div className="h-6 w-6 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+            <div className="h-6 w-6 bg-black rounded-full animate-bounce"></div>
+          </div>
+        </>
+      ) : (
+        <>
+          <Navbar handleOrderPopup={handleOrderPopup} />
+          <Outlet />
+          <Footer />
+          <OrderNow orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
+        </>
+      )}
     </>
   );
 };
